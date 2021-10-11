0f385b1c454b1d26ae29ea24e0f47cdb663a4de4
Heitor Paceli <heitorpaceli@gmail.com>
2021-10-11 20:54:09 -0300

    Add article 'Anatomy of an automated test script'

--- src/data/other-posts.js
+++ src/data/other-posts.js
@@ -7,6 +7,13 @@ const otherBlogPosts = [
     date: new Date("2021-08-10"),
     description: "Useful ADB Commands For Android Testing",
   },
+  {
+    title: "Anatomy of An Automated Test Script",
+    image:
+      "https://blog.testproject.io/wp-content/uploads/2021/08/Anatomy-of-a-Test-Script.png",
+    link: "https://blog.testproject.io/2021/09/22/anatomy-of-an-automated-test-script/",
+    date: new Date("2021-09-22"),
+  },
 ];
 
 export default otherBlogPosts;
